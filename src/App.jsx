88ebe47ebb99ad440794a2de72fbbcc1d40c6b4a
hlnvrnt <helene.vernet@live.fr>
2023-12-07 11:55:25 +0100

    tests techniques

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Form from './components/Form';
+
+function App() {
+ 
+  return (
+    <div className="app">
+      <Form/>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Form = () => {
+    const [contact, setContact] = useState({
+        name: "",
+        email: "",
+        message: "",
+      });
+      const [submittedContact, setSubmittedContact] = useState([]);
+    
+      const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setSubmittedContact([...submittedContact, contact]);
+		setContact({ name: "", email: "", message: "" });
+	};
+    
+    console.log(submittedContact)
+      return (
+        <div className="form-container">
+            <h1>Formulaire de contact</h1>
+        <div className="formulaire">
+            <form onSubmit={handleSubmit}>
+            <div className="saisi">
+            <div>
+            <input 
+            type="text"
+            name="name"
+            placeholder="Nom et Prénom"
+            value={contact.name}
+            onChange={(e) => setContact({ ...contact, name: e.target.value })}
+            />
+          </div>
+          <div>
+            <input 
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={contact.email}
+            onChange={(e) => setContact({ ...contact, email: e.target.value })}
+            />
+          </div>
+          <div className="message">
+            <input 
+            type="text"
+            name="Message"
+            placeholder="Message"
+            value={contact.message}
+            onChange={(e) => setContact({ ...contact, message: e.target.value })}
+            />
+          </div>
+          <div>
+            <button>Envoyer</button>
+          </div>
+        </div>    
+        </form>
+        {submittedContact.length > 0 && (
+						<div className="submitted-contact">
+							<table>
+                            <thead>
+                            <tr>
+                            <th colspan="1">Nom et Prénom</th>
+                            <th colspan="1">Adresse email</th>
+                            <th colspan="1">Message</th>
+                            </tr>
+                            </thead>
+								{submittedContact.map((info, index) => (
+									<tbody key={index}>
+                                        <tr>
+                                        <td>{info.name}</td>
+                                       <td>{info.email}</td>
+                                       <td>{info.message}</td>
+                                        </tr>
+                                    </tbody>
+								))}
+							</table>
+						</div>
+					)}
+
+         
+    </div>
+    </div>
+      )
+};
+
+export default Form;
